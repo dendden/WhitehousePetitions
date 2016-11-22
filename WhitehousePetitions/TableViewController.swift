335f668ac183ba342fe80504a506995151d1a377
Denys Triasunov <dendden@users.noreply.github.com>
2016-11-22 17:01:05 +0200

    'Popular' tab and view controller added.

--- WhitehousePetitions/TableViewController.swift
+++ WhitehousePetitions/TableViewController.swift
@@ -15,16 +15,23 @@ class TableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
+        let urlString: String
+        if navigationController?.tabBarItem.tag == 0 {
+            urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
+        } else {
+            urlString = "https://api.whitehouse.gov/v1/petitions.json?signatureCountFloor=10000&limit=100"
+        }
         if let url = URL(string: urlString) {
             if let data = try? Data(contentsOf: url) {
                 let json = JSON(data: data)
                 
                 if json["metadata"]["responseInfo"]["status"].intValue == 200 {
                     parse(json: json)
+                    return
                 }
             }
         }
+        showError()
     }
 
     func parse(json: JSON) {
@@ -38,6 +45,12 @@ class TableViewController: UITableViewController {
         }
         
         tableView.reloadData()
+    }
+    
+    func showError() {
+        let alert = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: .default))
+        present(alert, animated: true)
     }
 
     // MARK: - Table view data source
